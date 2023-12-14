8caf5aaf4819f8631fe9b72dd52e33a7a28a7bd9
Román Cárdenas <rcardenas.rod@gmail.com>
2023-12-14 13:01:20 +0100

    use HTTPS

---
+++ annsim2024.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+if __name__ == '__main__':
+    a = "xdevs-rs"
+    models = [['LI', 'HI', 'HO'], ['HOmod']]
+    sizes = [list(range(20, 401, 20)), list(range(5, 51, 5))]
+    for i, models in enumerate(models):
+        for m in models:
+            p = []
+            p.extend([f'{m}-{w}-{d}-0-0' for w in sizes[i] for d in sizes[i]])
+            p = ','.join(p)
+            n = 30
+            o = f"results_{m}.csv"
+
+            cmd = f"python3 devstone_comparative.py -a {a} -p {p} -n {n} -o {o}"
+            subprocess.run(cmd.split())
